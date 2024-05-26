961f74aa2b54aa701593440afd79db7ee11430c2
raquela <raquel.guzman@galileo.edu>
2024-05-26 17:16:31 -0600

    ajustes finales a todas las hojas y nav, y estilo responsive

--- efects.js
+++ efects.js
@@ -1,14 +1,14 @@
-/*==============    scrollReveall   =================*/
-ScrollReveal({
-    reset: true,
-    distance: '80px',
-    duration:2000,
-    delay: 200
-});
+/* variable para acceder  las secciones con la clase "hidden"  */
 
+const seccionesOcultas = document.querySelectorAll('.show');
 
-ScrollReveal().reveal('.info,', { origin: 'top'});
-ScrollReveal().reveal('.container-img, .section-clientes h2, .section-clientes h3, .slider', { origin: 'bottom'});
-ScrollReveal().reveal('.section-texto .container, .container-services, .info', { origin: 'right'});
-ScrollReveal().reveal('.section-services h2, .section-services h3, .footer, .section-conocenos .container', { origin: 'left'});
-ScrollReveal().reveal('.headline');
+//** observer */
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        entry.target.classList.toggle('mostrar', entry.
+        isIntersecting);
+    })
+})
+
+seccionesOcultas.forEach((seccion)=>observer.observe(seccion));    
--- main.js
+++ main.js
@@ -10,3 +10,13 @@ function scrollUp(){
 }
 
 
+
+ /*===================   efecto mostrar y esconder nav al hacer scrol    ====================*/
+ var zero = 0;
+ $(document).ready(function(){
+     $(window).on('scroll', function(){
+         $('.navbar').toggleClass('hide', $(window).scrollTop()
+         > zero);
+         zero = $(window).scrollTop();
+     })
+ })
